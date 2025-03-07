82ed14b7d2ef3051a3b0008fbd85b03b7a9e52a3
adesh-sri <53303727+adesh-sri@users.noreply.github.com>
2025-03-07 17:56:20 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,8 +1,1 @@
-const fetchAll = async (db, sql) => {
-  return new Promise((resolve, reject) => {
-    db.all(sql, (err, rows) => {
-      if (err) reject(err);
-      resolve(rows);
-    });
-  });
-};
+
