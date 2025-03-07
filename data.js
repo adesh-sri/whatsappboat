a9067ccba917de1680a1c75e9f9c40aad5bdae23
adesh-sri <53303727+adesh-sri@users.noreply.github.com>
2025-03-07 17:29:45 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,8 @@
+export const fetchAll = async (db, sql) => {
+  return new Promise((resolve, reject) => {
+    db.all(sql, (err, rows) => {
+      if (err) reject(err);
+      resolve(rows);
+    });
+  });
+};
